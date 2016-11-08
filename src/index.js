e17d41b19cbe436adb03fd884ed50ed616aff211
Иван Ганцев <igancev@MacBook-Pro-Ivan.local>
2016-11-09 01:44:34 +0300

    complete task2a

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
-app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
-  });
+
+app.get('/task2a', (req, res) => {
+  const summ = (+req.query.a || 0) + (+req.query.b || 0);
+  res.send(summ.toString());
 });
 
 app.listen(3000, () => {
